830f3bd771545dac8e01fd9cd43249411215f162
Mateus Amaral <mateus.almeida@dtidigital.com.br>
2022-02-18 12:31:14 -0300

    criação controller e service

---
+++ WebApiProduto/Services/Dto/ProdutoDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Dto
+{
+    public class ProdutoDto
+    {
+        public string IdProduto { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public decimal PrecoPromocao { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
---
+++ WebApiProduto/Services/Services/ProdutoService.cs
@@ -0,0 +1,34 @@
+﻿using Services.Dto;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class ProdutoService : IProdutoService
+    {
+
+        private readonly IProdutoRepository _produtoRepository;//colocar injecao dependencia
+
+
+        public ProdutoService(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public IEnumerable<ProdutoDto> ListarProdutoService()
+        {
+            try
+            {
+                return _produtoRepository.ListarProdutoRepository();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"ERROR: {ex.Message}");//conferir onde colocar exception
+            }
+        }
+    }
+}
