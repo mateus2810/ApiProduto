7ea97891b9184cb576f612f476720d0b184057a9
Mateus Amaral <mateus.almeida@dtidigital.com.br>
2022-02-23 10:09:17 -0300

    retirando idProduto do ProdutoDto

--- WebApiProduto/Domain/ProdutoDto.cs
+++ WebApiProduto/Domain/ProdutoDto.cs
@@ -7,7 +7,7 @@ namespace Domain
     [Table("dbo.produto ")]
     public class ProdutoDto
     {
-        public string IdProduto { get; set; }
+        //public string IdProduto { get; set; }
         public string Nome { get; set; }
         public decimal Preco { get; set; }
         public decimal PrecoPromocional { get; set; }
